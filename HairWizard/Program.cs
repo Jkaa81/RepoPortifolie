fdae495f3b5503f1fbe15aaf794832cba5c5c7bb
Jkaa81 <jkaa81@gmail.com>
2026-04-15 16:37:51 +0200

    Ensured migration when app starts

--- HairWizard/Program.cs
+++ HairWizard/Program.cs
@@ -33,13 +33,19 @@ namespace HairWizard
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                var db = scope.ServiceProvider.GetRequiredService<HairWizard.Data.HairWizardContext>();
+                db.Database.Migrate();
             }
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
